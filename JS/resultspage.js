6bcd2324b034b825bd140c7ed8f214a6fb040e62
terika <terika.collins@gmail.com>
2017-02-09 13:45:52 -0600

    created react component for results.js

--- JS/resultspage.js
+++ JS/resultspage.js
@@ -6,7 +6,17 @@ import { hashHistory, Link } from 'react-router'
 export default React.createClass ({
   render () {
     return (
-
+      <section>
+      <article className="row featurette">
+        <h2 className="featurette-heading"> Name of Entree </h2>
+        <h2 className="text-muted"> Name of the Restaurant</h2>
+          <img
+            className="featurette-image  img-responsive center-block"
+            src="http://images.media-allrecipes.com/images/51552.jpg"
+            alt="shrimp"/>
+          <p><Link className="btn btn-primary btn-lg" to=""> RESERVATIONS »</Link></p>
+      </article>
+    </section>
     )
   }
 })
