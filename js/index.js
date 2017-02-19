81e1f104c3165d4413b7a3e835db949568341097
terika <terika.collins@gmail.com>
2017-02-19 00:52:38 -0600

    created boilerplate

--- js/index.js
+++ js/index.js
@@ -3,22 +3,14 @@ import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import jquery from 'jquery'
 import app from './app'
-import home from './home'
-import searchpage from './searchpage'
-import resultspage from './resultspage'
-import dataForm from './dataForm'
-import carousel from './carousel'
+import resume from './resume'
 
 
 render(
   (
     <Router history={hashHistory}>
       <Route component={app}>
-        <Route path="/" component={ home } />
-        <Route path="/searchpage" component={ searchpage } />
-        <Route path="/resultspage" component={ resultspage } />
-        <Route path="/dataForm" component={ dataForm }/>
-        <Route path="/carousel" component={ carousel }/>
+        <Route path="/" component={ resume } />
       </Route>
     </Router>
   ),
---
+++ js/resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom'
+import { hashHistory, Link } from 'react-router'
+
+
+export default React.createClass ({
+
+  },
+  render () {
+    return (
+    <Section />
+  }
+})
