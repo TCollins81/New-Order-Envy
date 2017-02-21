a7f2e20c1df26f04e19a352c74b57c34a17c1f6a
terika <terika.collins@gmail.com>
2017-02-21 11:20:39 -0600

    modified txt on home and search page. tested the footer for asthetics and decided against it.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Header from './Header'
-import Footer from './Footer'
+import Header from './header'
 
 export default React.createClass({
   render() {
@@ -9,7 +8,6 @@ export default React.createClass({
     <main>
       <Header />
       {this.props.children}
-      <Footer />
     </main>
     )
   }
--- js/home.js
+++ js/home.js
@@ -15,7 +15,7 @@ export default React.createClass ({
         <div className="jumbotron">
           <div className="container">
             <h1> DONT GET MAD... GET ENVY. </h1>
-            <p>Have you ever placed an order at a restaurant and instantly felt regret when you see someone else's food arrive at their table? Let us, help you! Search for the most popular entrees where you are dining and never have ORDER ENVY again... so what are you waiting for?</p>
+            <p>Have you ever placed an order at a restaurant and instantly felt regret when you see someone else's food arrive at their table? Let us, help you! Search for the most popular entrees at The Pearl Brewery and never have ORDER ENVY again... so what are you waiting for?</p>
             <br/>
             <p><Link className="btn btn-primary btn-lg" to="/searchpage">LETS GET STARTED »</Link></p>
           </div>
@@ -30,7 +30,7 @@ export default React.createClass ({
             <div className="col-md-4">
               <span className="glyphicon glyphicon-leaf glyphicon-large" aria-hidden="true"></span>
               <h3> Special Dietary Restrictions </h3>
-              <p> Search for Vegetarian and Gluten Free options.</p>
+              <p> Search for Vegetarian and Gluten Friendly options.</p>
             </div>
             <div className="col-md-4">
               <span className="glyphicon glyphicon-globe glyphicon-large" aria-hidden="true"></span>
